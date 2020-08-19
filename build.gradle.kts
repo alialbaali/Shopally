d9086bdc8e648c75646e8b03969244e9403ece90
Ali Albaali <alialbaali@pm.me>
2020-08-19 19:49:13 +0300

    Update Gradle dependencies and versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val koin_version: String by project
+val kotest_version : String by project
 
 buildscript {
     repositories {
@@ -16,7 +17,7 @@ buildscript {
 
 plugins {
     application
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.0"
     id("com.squareup.sqldelight") version "1.4.0"
 }
 
@@ -27,7 +28,7 @@ sqldelight {
 }
 
 group = "com.shopping"
-version = "0.0.1"
+version = "0.0.5"
 
 application {
     mainClassName = "io.ktor.server.cio.EngineMain"
@@ -41,7 +42,6 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     implementation("io.ktor:ktor-server-cio:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
@@ -49,26 +49,40 @@ dependencies {
     implementation("io.ktor:ktor-auth:$ktor_version")
     implementation("io.ktor:ktor-auth-jwt:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("io.ktor:ktor-locations:$ktor_version")
     implementation("com.squareup.sqldelight:sqlite-driver:1.4.0")
     implementation("org.koin:koin-ktor:2.1.6")
+//    implementation("com.stripe:stripe-java:19.40.0")
 
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("com.squareup.sqldelight:sqlite-driver:1.4.0")
     testImplementation("org.koin:koin-test:2.1.6")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.3")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.1.3")
-    testImplementation("io.kotest:kotest-property-jvm:4.1.3")
-    testImplementation("io.kotest:kotest-runner-console-jvm:4.1.3")
-    testImplementation("io.kotest:kotest-assertions-ktor-jvm:4.1.3")
-    testImplementation("io.kotest:kotest-extensions-koin-jvm:4.1.3")
+
+
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-property-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-assertions-ktor-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-extensions-koin-jvm:$kotest_version")
+
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 tasks {
     compileKotlin {
         kotlinOptions {
-            freeCompilerArgs = listOf("-Xallow-result-return-type")
-            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xallow-result-return-type", "-Xinline-classes")
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
+        }
+    }
+    compileTestKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xallow-result-return-type", "-Xinline-classes")
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
         }
     }
 
