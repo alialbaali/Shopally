e2f7c268970b761a13825bc554266852975f0484
Ali Albaali <alialbaali@pm.me>
2020-08-21 21:11:36 +0300

    Add jwt payload extension variable

--- src/main/kotlin/com/shopping/Utils.kt
+++ src/main/kotlin/com/shopping/Utils.kt
@@ -1,6 +1,13 @@
 package com.shopping
 
+import com.auth0.jwt.interfaces.Payload
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
+import io.ktor.features.*
+import io.ktor.locations.*
 import io.ktor.util.*
+import io.ktor.util.pipeline.*
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.*
@@ -19,3 +26,45 @@ fun String.hash(): String {
 fun Date.toLocalDateTime(): LocalDateTime = LocalDateTime.ofInstant(this.toInstant(), ZoneId.systemDefault())
 
 fun LocalDateTime.toDate(): Date = Date.from(this.atZone(ZoneId.systemDefault()).toInstant());
+
+inline val PipelineContext<*, ApplicationCall>.jwtPayload: Payload
+    get() = call.principal<JWTPrincipal>()?.payload ?: throw BadRequestException(Errors.INVALID_REQUEST)
+
+//fun User.sendEmail() {
+//    try {
+//
+//        val username = System.getenv("EMAIL")
+//        val password = System.getenv("PASSWORD")
+//        val recipient = this.userEmail
+//
+//
+//        val host = "smtp.gmail.com"
+//        val properties = System.getProperties().also { props ->
+//            props["mail.smtp.starttls.enable"] = "true"
+//            props["mail.smtp.host"] = host
+//            props["mail.smtp.user"] = username
+//            props["mail.smtp.password"] = password
+//            props["mail.smtp.port"] = "587"
+//            props["mail.smtp.auth"] = "true"
+//        }
+//
+//        val session = Session.getInstance(properties)
+//
+//        val message = MimeMessage(session)
+//
+//
+//        message.setFrom("noreply@noto.com")
+//        message.setRecipient(Message.RecipientType.TO, InternetAddress(recipient))
+//
+//        message.subject = "Verification"
+//        message.setText("HELLO")
+//        val transport = session.getTransport("smtp")
+//        transport.connect(host, username, password)
+//        transport.sendMessage(message, message.allRecipients)
+//        transport.close()
+//
+//    } catch (e: Throwable) {
+//        println(e.toString())
+//    }
+//
+//}
