bba21c6d04b747de8f3fee7a54c5307e49185002
Ali Albaali <alialbaali@pm.me>
2020-08-31 23:19:29 +0300

    Add Stripe Customer Data Source

---
+++ src/main/kotlin/com/shopping/domain/StripeCustomerDataSource.kt
@@ -0,0 +1,17 @@
+package com.shopping.domain
+
+import com.stripe.model.Customer
+
+typealias StripeCustomer = Customer
+
+interface StripeCustomerDataSource {
+
+    suspend fun getStripeCustomerById(stripeCustomerId: String): Result<StripeCustomer>
+
+    suspend fun createStripeCustomer(name: String, email: String): Result<StripeCustomer>
+
+    suspend fun updateStripeCustomerById(stripeCustomerId: String, name: String, email: String): Result<StripeCustomer>
+
+    suspend fun deleteStripeCustomerById(stripeCustomerId: String): Result<StripeCustomer>
+
+}
