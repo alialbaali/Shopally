33e5ef07cb4d49d492f4075d87b8f38be807de7e
Ali Albaali <alialbaali@pm.me>
2020-09-04 10:28:46 +0300

    Add KtLint, Mockk and Update Gradle version to 6.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val koin_version: String by project
-val kotest_version : String by project
+val kotest_version: String by project
 
 buildscript {
     repositories {
         google()
+        jcenter()
         mavenCentral()
     }
     dependencies {
@@ -14,11 +15,11 @@ buildscript {
     }
 }
 
-
 plugins {
     application
     kotlin("jvm") version "1.4.0"
     id("com.squareup.sqldelight") version "1.4.0"
+    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
 }
 
 sqldelight {
@@ -28,7 +29,17 @@ sqldelight {
 }
 
 group = "com.shopping"
-version = "0.0.5"
+version = "0.2"
+
+ktlint {
+    version.set("0.38.1")
+    coloredOutput.set(true)
+    disabledRules.set(mutableListOf("no-wildcard-imports"))
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
 
 application {
     mainClassName = "io.ktor.server.cio.EngineMain"
@@ -51,21 +62,20 @@ dependencies {
     implementation("io.ktor:ktor-auth-jwt:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
     implementation("io.ktor:ktor-locations:$ktor_version")
-    implementation("com.squareup.sqldelight:sqlite-driver:1.4.0")
+    implementation("com.squareup.sqldelight:sqlite-driver:1.4.2")
     implementation("org.koin:koin-ktor:2.1.6")
     implementation("com.cloudinary:cloudinary-http44:1.26.0")
-//    implementation("com.stripe:stripe-java:19.40.0")
+    implementation("com.stripe:stripe-java:19.45.0")
 
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("com.squareup.sqldelight:sqlite-driver:1.4.0")
     testImplementation("org.koin:koin-test:2.1.6")
-
+    testImplementation("io.mockk:mockk:1.10.0")
 
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotest_version")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotest_version")
     testImplementation("io.kotest:kotest-property-jvm:$kotest_version")
     testImplementation("io.kotest:kotest-assertions-ktor-jvm:$kotest_version")
-
 }
 
 java {
