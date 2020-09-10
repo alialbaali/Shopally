4bf9a5befaf8a76315e31e849bcce370041f9ce6
Ali Albaali <alialbaali@pm.me>
2020-09-10 14:25:13 +0300

    Refactor DTO classes to request and response packages

--- test/com/shopping/fake/FakeCustomerRepository.kt
+++ test/com/shopping/fake/FakeCustomerRepository.kt
@@ -1,15 +1,14 @@
 package com.shopping
 
 import com.shopping.domain.model.Customer
-import com.shopping.domain.model.valueObject.Address
-import com.shopping.domain.model.valueObject.Card
-import com.shopping.domain.model.valueObject.Email
-import com.shopping.domain.model.valueObject.ID
+import com.shopping.domain.model.Order
+import com.shopping.domain.model.valueObject.*
 import com.shopping.domain.repository.CustomerRepository
 import java.io.File
 
 class FakeCustomerRepository(
     private val customers: MutableList<Customer> = mutableListOf(),
+    private val carts: MutableMap<ID, Cart> = mutableMapOf(),
     private val addresses: MutableMap<ID, Address> = mutableMapOf(),
     private val cards: MutableMap<ID, Card> = mutableMapOf(),
     private val cloudDataSource: MutableMap<ID, File> = mutableMapOf(),
@@ -30,8 +29,9 @@ class FakeCustomerRepository(
     }
 
     override suspend fun createCustomer(customer: Customer): Result<Customer> {
-        return if (customers.any { it.email == customer.email })
+        return if (customers.any { it.email.toString().toLowerCase() == customer.email.toString().toLowerCase() }){
             Result.failure(Throwable(Errors.INVALID_EMAIL))
+        }
         else {
             customers.add(customer)
             Result.success(customer)
@@ -50,7 +50,7 @@ class FakeCustomerRepository(
         }
     }
 
-    override suspend fun updateCustomerImageById(customerId: ID, imageFile: File): Result<String> {
+    override suspend fun updateCustomerImage(customerId: ID, imageFile: File): Result<String> {
 
         val customer = getCustomerById(customerId).getOrElse {
             return Result.failure(it)
@@ -67,9 +67,40 @@ class FakeCustomerRepository(
         else
             Result.failure(Throwable(Errors.INVALID_ID))
 
+    override suspend fun getCartByCustomerId(customerId: ID): Result<Cart> {
+        val entry = carts.entries.find { entry -> entry.key == customerId } ?: return Result.failure(Throwable())
+        return Result.success(entry.value)
+    }
+
+    override suspend fun createCartItem(customerId: ID, orderItem: Order.OrderItem): Result<Order.OrderItem> {
+        carts[customerId] = Cart(carts[customerId]?.value?.plus(orderItem) ?: return Result.failure(Throwable()))
+        return Result.success(orderItem)
+    }
+
+    override suspend fun updateCartItem(customerId: ID, orderItem: Order.OrderItem): Result<Order.OrderItem> {
+
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCartItem(customerId: ID, productID: ID): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun emptyCartByCustomerId(customerId: ID): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAddressByName(customerId: ID, addressName: String): Result<Address> {
+        TODO("Not yet implemented")
+    }
+
     override suspend fun countAddressesByCustomerId(customerId: ID): Result<Long> {
         val addressesCount = addresses.count { entry -> entry.key == customerId }
         return Result.success(addressesCount.toLong())
+    }
+
+    override suspend fun getCardByNumber(customerId: ID, cardNumber: Long): Result<Card> {
+        TODO("Not yet implemented")
     }
 
     override suspend fun countCardsByCustomerId(customerId: ID): Result<Long> {
