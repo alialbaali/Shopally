a1253fa5ea7f5db6126db382034bba6fb8da2e08
Ali Albaali <alialbaali@pm.me>
2020-09-13 16:53:05 +0300

    Add OrderItems DB table

--- test/com/shopping/queries/OrderItemsQueriesTest.kt
+++ test/com/shopping/queries/OrderItemsQueriesTest.kt
@@ -4,17 +4,13 @@ import com.shopping.DefaultSpec
 import com.shopping.dataSourceModule
 import com.shopping.db.OrderItemsQueries
 import com.shopping.dbModule
-import com.shopping.domain.model.Order
 import com.shopping.domain.model.valueObject.ID
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrowAny
-import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldExist
-import io.kotest.matchers.collections.shouldHaveSize
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
 import io.kotest.matchers.string.shouldContainIgnoringCase
 import org.koin.test.inject
-import kotlin.random.Random
 
 class OrderItemsQueriesTest : DefaultSpec(dbModule, dataSourceModule) {
 
@@ -29,15 +25,13 @@ class OrderItemsQueriesTest : DefaultSpec(dbModule, dataSourceModule) {
                     val orderId = ID.random()
                     val productId = ID.random()
                     val quantity = 3L
-                    val price = 99.99
 
                     shouldNotThrowAny {
-                        orderItemsQueries.createOrderItem(orderId, productId, quantity, price)
+                        orderItemsQueries.createOrderItem(orderId, productId, quantity)
                     }
 
-                    val orderItems = orderItemsQueries.getOrderItemsByOrderId(orderId).executeAsList()
-
-                    orderItems.shouldExist { it.product_id == productId && it.quantity == quantity && it.price == price }
+                    orderItemsQueries.getOrderItemsByOrderId(orderId)
+                        .executeAsList() shouldExist { it.product_id == productId && it.quantity == quantity }
                 }
             }
         }
@@ -47,44 +41,22 @@ class OrderItemsQueriesTest : DefaultSpec(dbModule, dataSourceModule) {
                 When("creating all of them using the same order id") {
                     Then("we should be able to query all of them by the same order id") {
 
-                        val orderItems = mutableListOf<Order.OrderItem>()
-
                         val orderId = ID.random()
-
-                        repeat(10) {
-
-                            val orderItem = Order.OrderItem(
-                                productId = ID.random(),
-                                quantity = it.toLong(),
-                                price = it.times(it).times(Random.nextDouble(100.0)),
-                            )
-
-                            orderItems.add(orderItem)
-                        }
 
                         shouldNotThrowAny {
 
-                            orderItems.forEach { orderItem ->
+                            repeat(10) {
+
                                 orderItemsQueries.createOrderItem(
                                     orderId,
-                                    orderItem.productId,
-                                    orderItem.quantity,
-                                    orderItem.price
+                                    ID.random(),
+                                    it.times(2L)
                                 )
                             }
                         }
 
-                        val dbOrderItems =
-                            orderItemsQueries.getOrderItemsByOrderId(orderId) { _, product_id, quantity, price ->
-                                Order.OrderItem(
-                                    product_id,
-                                    quantity,
-                                    price
-                                )
-                            }.executeAsList().sortedBy { it.quantity }
-
-                        dbOrderItems shouldHaveSize orderItems.size
-                        dbOrderItems shouldBe orderItems
+                        orderItemsQueries.getOrderItemsByOrderId(orderId)
+                            .executeAsList() shouldHaveAtLeastSize 10
                     }
                 }
             }
@@ -97,35 +69,19 @@ class OrderItemsQueriesTest : DefaultSpec(dbModule, dataSourceModule) {
 
                         val productId = ID.random()
 
-                        val orderItem = Order.OrderItem(
-                            productId = productId,
-                            quantity = 3,
-                            price = 3.times(Random.nextDouble(100.0)),
-                        )
-
                         shouldNotThrowAny {
 
                             repeat(10) {
                                 orderItemsQueries.createOrderItem(
                                     ID.random(),
-                                    orderItem.productId,
-                                    orderItem.quantity,
-                                    orderItem.price
+                                    productId,
+                                    it.times(2L),
                                 )
                             }
                         }
 
-                        val dbOrderItems =
-                            orderItemsQueries.getOrderItemsByProductId(productId) { _, product_id, quantity, price ->
-                                Order.OrderItem(
-                                    product_id,
-                                    quantity,
-                                    price
-                                )
-                            }.executeAsList().sortedBy { it.quantity }
-
-                        dbOrderItems shouldHaveSize 10
-                        dbOrderItems.all { it == orderItem }.shouldBeTrue()
+                        orderItemsQueries.getOrderItemsByProductId(productId)
+                            .executeAsList() shouldHaveAtLeastSize 10
                     }
                 }
             }
@@ -133,32 +89,19 @@ class OrderItemsQueriesTest : DefaultSpec(dbModule, dataSourceModule) {
 
         Given("a list of order items with a non-unique order and product ids") {
             When("creating all of them using the same product id and the same order id") {
-                Then("it should fail due unique constraint") {
-
-                    val orderItems = mutableListOf<Order.OrderItem>()
+                Then("it should fail due to unique constraint") {
 
                     val orderId = ID.random()
                     val productId = ID.random()
 
-                    repeat(10) {
-
-                        val orderItem = Order.OrderItem(
-                            productId = productId,
-                            quantity = it.toLong(),
-                            price = it.times(it).times(Random.nextDouble(100.0)),
-                        )
-
-                        orderItems.add(orderItem)
-                    }
-
                     shouldThrowAny {
 
-                        orderItems.forEach { orderItem ->
+                        repeat(10) {
+
                             orderItemsQueries.createOrderItem(
                                 orderId,
-                                orderItem.productId,
-                                orderItem.quantity,
-                                orderItem.price
+                                productId,
+                                it.times(2L),
                             )
                         }
                     }.message shouldContainIgnoringCase "UNIQUE CONSTRAINT FAILED"
