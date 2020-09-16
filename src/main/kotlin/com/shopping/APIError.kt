c8435d0f37ae3ff2389a62395cc412fe66d314af
Ali Albaali <alialbaali@pm.me>
2020-09-16 17:38:50 +0300

    Add functions for API Error classes

--- src/main/kotlin/com/shopping/APIError.kt
+++ src/main/kotlin/com/shopping/APIError.kt
@@ -6,36 +6,56 @@ sealed class APIError(override val message: String?) : RuntimeException(message)
     abstract val statusCode: HttpStatusCode
 }
 
-class AuthenticationError(message: String?) : APIError(message) {
+fun authenticationError(message: String?): Nothing = throw AuthenticationError(message)
+private class AuthenticationError(message: String?) : APIError(message) {
     override val statusCode: HttpStatusCode
         get() = HttpStatusCode.Unauthorized
 }
 
-class AuthorizationError(message: String?) : APIError(message) {
+fun authorizationError(message: String?): Nothing = throw AuthorizationError(message)
+private class AuthorizationError(message: String?) : APIError(message) {
     override val statusCode: HttpStatusCode
         get() = HttpStatusCode.Forbidden
 }
 
-class NotFoundError(message: String?) : APIError(message) {
-    override val statusCode: HttpStatusCode
-        get() = HttpStatusCode.NotFound
-}
-
-class BadRequestError(message: String?) : APIError(message) {
+fun badRequestError(message: String?): Nothing = throw BadRequestError(message)
+private class BadRequestError(message: String?) : APIError(message) {
     override val statusCode: HttpStatusCode
         get() = HttpStatusCode.BadRequest
 }
 
+fun notFoundError(message: String?): Nothing = throw NotFoundError(message)
+private class NotFoundError(message: String?) : APIError(message) {
+    override val statusCode: HttpStatusCode
+        get() = HttpStatusCode.NotFound
+}
+
+fun internalServerError(message: String?): Nothing = throw InternalServerError(message)
+private class InternalServerError(message: String?) : APIError(message) {
+    override val statusCode: HttpStatusCode
+        get() = HttpStatusCode.InternalServerError
+}
+
 object Errors {
 
-    const val INVALID_NAME = "Invalid name"
-    const val INVALID_CREDENTIALS = "Invalid credentials"
-    const val INVALID_REQUEST = "Invalid request"
-    const val INVALID_TOKEN = "Invalid token"
-    const val INVALID_EMAIL = "Invalid Email-Address"
-    const val INVALID_PASSWORD = "Invalid password"
-    const val PASSWORD_VALIDATION = "Password must contain at least 8 characters and one number"
-    const val INVALID_IMAGE = "Invalid image url"
-    const val INVALID_ISBN = "Invalid ISBN"
-    const val INVALID_ID = "Invalid ID"
+    const val InvalidName = "Invalid name. Please provide a valid name!"
+    const val InvalidCredentials = "Invalid credentials. Please provide valid credentials"
+    const val InvalidRequest = "Invalid request"
+    const val InvalidToken = "Invalid token. Please provide a valid token; if you don't have one, you can get by signing up though /auth/new"
+    const val InvalidEmail = "Invalid Email address. Please provide a valid email address"
+    const val InvalidPassword = "Invalid password. Please provide a valid password; password must contain at least 8 characters and 1 number"
+    const val PasswordValidation = "Password must contain at least 8 characters and one number"
+    const val InvalidId = "Invalid ID"
+    const val SomethingWentWrong = "Something went wrong!"
+    const val CustomerDoesntExist = "Customer doesn't exist"
+    const val UsedEmail = "Email not available"
+    const val ProductDoesntExist = "Product doesn't exist"
+    const val CartItemDoesntExist = "Cart item doesn't exist"
+    const val AddressDoesntExist = "Address doesn't exist"
+    const val CardDoesntExist = "Card doesn't exist"
+    const val OrderDoesntExist = "Order doesn't exist"
+    const val ImageUrlDoesntExist = "Image Url doesn't exist"
+    const val ReviewDoesntExist = "Review doesn't exist"
+    const val ImageUploadFailed = "Image upload failed"
+    const val LimitSize = "Maximum limit is 100 and default is 50"
 }
